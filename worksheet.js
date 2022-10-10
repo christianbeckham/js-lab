fcf678d5775fc2cf191963e5e2d8c9e2244fe21e
Christian Beckham <urenacb22@gmail.com>
2022-10-10 13:16:28 -0500

    added code for task 2 implementation

--- worksheet.js
+++ worksheet.js
@@ -6,3 +6,9 @@ dayOfWeek = "Friday";
 console.log(`I can't wait for ${dayOfWeek}!`);
 
 // Task 2: User Input
+let animalInput = prompt("What is your favorite animal?");
+let colorInput = prompt("What is your favorite color?");
+
+let outputString = `I've never seen a ${colorInput} ${animalInput}!`;
+
+console.log(outputString);
